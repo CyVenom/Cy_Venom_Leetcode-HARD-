57c9f3314764ca428286dcd3829fa36ee57f5acd
CyVen <90514507+CyVenom@users.noreply.github.com>
2025-03-20 15:50:59 +0800

    Create Longest Valid Parentheses.cpp

---
+++ Longest Valid Parentheses.cpp
@@ -0,0 +1,25 @@
+#Given a string containing just the characters '(' and ')', return the length of the longest valid (well-formed) parentheses substring.
+
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        stack<int> st;
+        st.push(-1);  
+        int maxLen = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(') {
+                st.push(i);
+            } else {
+                st.pop();
+                if (st.empty()) {
+                    st.push(i);
+                } else {
+                    maxLen = max(maxLen, i - st.top());
+                }
+            }
+        }
+
+        return maxLen;
+    }
+};
