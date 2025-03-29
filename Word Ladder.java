2ffe883cee97a57d740d7788d7d7fd00e6c7608a
CyVen <90514507+CyVenom@users.noreply.github.com>
2025-03-29 16:55:38 +0800

    Create Word Ladder.java

---
+++ Word Ladder.java
@@ -0,0 +1,48 @@
+
+
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> wordSet = new HashSet<>(wordList);
+        if (!wordSet.contains(endWord)) return 0;
+
+        Queue<String> queue = new LinkedList<>();
+        queue.add(beginWord);
+        int level = 1;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                String word = queue.poll();
+                if (word.equals(endWord)) return level;
+                
+                for (String neighbor : getNeighbors(word, wordSet)) {
+                    queue.add(neighbor);
+                    wordSet.remove(neighbor);
+                }
+            }
+            level++;
+        }
+
+        return 0;
+    }
+
+    private List<String> getNeighbors(String word, Set<String> wordSet) {
+        List<String> neighbors = new ArrayList<>();
+        char[] chars = word.toCharArray();
+
+        for (int i = 0; i < chars.length; i++) {
+            char originalChar = chars[i];
+            for (char c = 'a'; c <= 'z'; c++) {
+                if (c == originalChar) continue;
+                chars[i] = c;
+                String newWord = new String(chars);
+                if (wordSet.contains(newWord)) {
+                    neighbors.add(newWord);
+                }
+            }
+            chars[i] = originalChar;
+        }
+
+        return neighbors;
+    }
+}
