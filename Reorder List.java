8334ca722ac9d53ca34a448d3e8946546749971d
CyVen <90514507+CyVenom@users.noreply.github.com>
2025-03-30 21:12:03 +0800

    Create Reorder List.java

---
+++ Reorder List.java
@@ -0,0 +1,43 @@
+class Solution {
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null) {
+            return; 
+        }
+
+       
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        
+        ListNode second = reverseList(slow.next);
+        slow.next = null; 
+
+       
+        ListNode first = head;
+        while (second != null) {
+            ListNode temp1 = first.next;
+            ListNode temp2 = second.next;
+
+            first.next = second;
+            second.next = temp1;
+
+            first = temp1;
+            second = temp2;
+        }
+    }
+
+   
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null, curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+}
