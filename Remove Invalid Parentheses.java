ccc7b1807882cd8b76e6e4d87ea4ce946b174982
CyVen <90514507+CyVenom@users.noreply.github.com>
2025-04-09 12:04:00 +0800

    Create Remove Invalid Parentheses.java

---
+++ Remove Invalid Parentheses.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<String> removeInvalidParentheses(String s) {
+        Set<String> result = new HashSet<>();
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(s);
+        boolean foundValid = false;
+        
+        while (!queue.isEmpty()) {
+            String curr = queue.poll();
+            if (isValid(curr)) {
+                result.add(curr);
+                foundValid = true;
+            }
+            if (!foundValid) {
+                for (int i = 0; i < curr.length(); i++) {
+                    if (curr.charAt(i) == '(' || curr.charAt(i) == ')') {
+                        String next = curr.substring(0, i) + curr.substring(i + 1);
+                        queue.offer(next);
+                    }
+                }
+            }
+        }
+        
+        return new ArrayList<>(result);
+    }
+    
+    private boolean isValid(String s) {
+        int count = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(') count++;
+            if (c == ')') count--;
+            if (count < 0) return false;
+        }
+        return count == 0;
+    }
+}
