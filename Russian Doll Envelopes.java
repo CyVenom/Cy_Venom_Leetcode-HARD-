02e3179dcf2c49ff5a22bd701674bee34a3f65ff
CyVen <90514507+CyVenom@users.noreply.github.com>
2025-04-13 23:02:48 +0800

    Create Russian Doll Envelopes.java

---
+++ Russian Doll Envelopes.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxEnvelopes(int[][] envelopes) {
+        
+        Arrays.sort(envelopes, (a, b) -> 
+            a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]
+        );
+
+        
+        int[] heights = new int[envelopes.length];
+        for (int i = 0; i < envelopes.length; i++)
+            heights[i] = envelopes[i][1];
+
+     
+        return lengthOfLIS(heights);
+    }
+
+    private int lengthOfLIS(int[] nums) {
+        List<Integer> lis = new ArrayList<>();
+        for (int num : nums) {
+            int idx = Collections.binarySearch(lis, num);
+            if (idx < 0) idx = -(idx + 1);
+            if (idx == lis.size()) lis.add(num);
+            else lis.set(idx, num);
+        }
+        return lis.size();
+    }
+}
